9473fbf8336f4b51a4e23e23e2bfd1975206f43e
Arius0476-hash <arius0476@gmail.com>
2026-03-31 22:38:34 +0100

    Create astro.config.mjs

---
+++ astro.config.mjs
@@ -0,0 +1,6 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://arius0476-hash.github.io',
+  base: '/mon-portfolio',  // Assure-toi que le nom ici est EXACTEMENT celui de ton dépôt GitHub
+});
